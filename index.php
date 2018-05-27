ed624eef40cefcbc4228f0fbea7dbd2ef198755d
Friendly Halomoan Sipayung <friendly.1301144001@gmail.com>
2018-05-27 16:27:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+   <?php
+    $addresses = [
+      'https://cnhv.co/3w5s6',
+      'https://cnhv.co/3w5s6',
+      'http://www.frie.me',
+      'http://www.frie.me',
+      'http://www.frie.me',
+    ];
+    $size = count($addresses);
+    $randomIndex = rand(0, $size - 1);
+    $randomUrl = $addresses[$randomIndex];
+    header('Location: ' . $randomUrl, true, 303);
+    
